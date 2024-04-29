497a9667248e18a33c5cf91d59012ab49fb2aced
mz1874 <mz1874@aliyun.com>
2024-04-30 01:39:01 +0800

    #跨域允许

--- App/extension/__init__.py
+++ App/extension/__init__.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_caching import Cache
+from flask_cors import CORS
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -9,6 +10,7 @@ cache = Cache(config={'CACHE_TYPE': 'simple'})
 
 
 def init_extension(app):
+    CORS(app)
     db.init_app(app=app)
     cache.init_app(app=app)
     migrate.init_app(app=app, db=db)
