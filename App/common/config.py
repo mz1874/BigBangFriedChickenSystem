7662612c9067e232baa92e1ffef12ff42ad6a59c
mz1874 <mz1874@aliyun.com>
2024-05-08 23:48:49 +0800

    #食物新增完成， 文件上传完成

---
+++ App/common/config.py
@@ -0,0 +1,5 @@
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
+UPLOAD_FOLDER = r"E:\upload"
+
+# LINUX_UPLOAD_FOLDER = ''
--- App/controllers/food_view.py
+++ App/controllers/food_view.py
@@ -1,10 +1,17 @@
 from flask import Blueprint, jsonify, request
-from App.common.decorators import requires_permission
-from App.extension import db
+import os
 from App.common.common_response import CommonResponse
+from App.models.food_model import FoodModel
+from werkzeug.utils import secure_filename
+from App.common.config import UPLOAD_FOLDER, ALLOWED_EXTENSIONS
+from App.extension import db
 from App.models.food_category_model import FoodCategory
 
 food_view = Blueprint('food', __name__)
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @food_view.route("/food/page", methods=["GET"])
@@ -14,7 +21,48 @@ def page():
 
 @food_view.route("/food/add", methods=["POST"])
 def food_add():
-    pass
+    request_data = request.get_json()
+    if request_data is not None:
+        food_name = request_data.get("foodName")
+        src = request_data.get("src")
+        food_category_id = request_data.get("foodCategoryId")
+        price = request_data.get("price")
+        if not all([food_name, src, food_category_id, price]):
+            return CommonResponse.failure("All fields are required")
+        else:
+            category = FoodCategory.query.filter_by(id=food_category_id).first()
+            if category is not None:
+                food = FoodModel()
+                food.food_name = food_name
+                food.food_category = category
+                food.price = price
+                food.img = src
+                try:
+                    db.session.add(food)
+                    db.session.commit()
+                    return CommonResponse.success("Add food successfully")
+                except Exception as e:
+                    db.session.rollback()
+                    db.session.rollback()
+                    return CommonResponse.failure("Add food error", 400)
+            else:
+                return CommonResponse.failure("Food category error", 400)
+
+
+@food_view.route('/food/upload', methods=['POST'])
+def food_image_upload():
+    if 'foodImage' not in request.files:
+        return jsonify({"message": "No file part"}), 400
+    file = request.files['foodImage']
+    if file.filename == '':
+        return jsonify({"message": "No selected file"}), 400
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(UPLOAD_FOLDER, filename))
+        file_path = os.path.join(UPLOAD_FOLDER, filename)
+        return jsonify({"message": "File uploaded successfully", "file_path": file_path}), 200
+    else:
+        return jsonify({"message": "Invalid file type"}), 400
 
 
 @food_view.route("/food/delete", methods=["POST"])
